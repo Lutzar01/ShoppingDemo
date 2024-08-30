d8443c151589e9509f9ee1ad09cc56443e9d0fdd
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-08-29 20:05:46 -0700

    Initial Commit
    spring initializer files

---
+++ src/test/java/com/lutzarDemos/shoppingdemo/ShoppingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutzarDemos.shoppingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
