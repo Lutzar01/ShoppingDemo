4367ea8158d200877301f991152617d5306b36f7
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-08-30 23:05:01 -0700

    Update Product.java
    create constructer for class

--- src/main/java/com/lutzarDemos/shoppingdemo/model/Product.java
+++ src/main/java/com/lutzarDemos/shoppingdemo/model/Product.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Product {
     @Id
@@ -34,4 +33,13 @@ public class Product {
     // When an image is deleted, all the images related to that product will be removed
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Image> imageList;
+
+    public Product(String name, String brand, BigDecimal price, int inventory, String description, Category category) {
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.inventory = inventory;
+        this.description = description;
+        this.category = category;
+    }
 }
