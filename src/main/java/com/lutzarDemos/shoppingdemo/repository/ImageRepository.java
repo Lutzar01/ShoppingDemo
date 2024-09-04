a7e5e32bb84b67214a8e61a5d7a377d9e193e533
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-03 21:14:58 -0700

    Updated Repositories w/ Documentation
    Updated:
    - CategoryRespository.java
    - ImageRespository.java
    - ProductRespository.java

--- src/main/java/com/lutzarDemos/shoppingdemo/repository/ImageRepository.java
+++ src/main/java/com/lutzarDemos/shoppingdemo/repository/ImageRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// Manages image entity and saves data to the MySQL DB
 public interface ImageRepository extends JpaRepository<Image, Long> {
     List<Image> findByProductId(Long id);
 
