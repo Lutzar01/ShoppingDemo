0d0da2d152f74745a67e70ac24a4837300f8a094
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-03 21:18:03 -0700

    Update ApiResponse.java
    added documentation

--- src/main/java/com/lutzarDemos/shoppingdemo/response/ApiResponse.java
+++ src/main/java/com/lutzarDemos/shoppingdemo/response/ApiResponse.java
@@ -3,6 +3,7 @@ package com.lutzarDemos.shoppingdemo.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+// Handles HTTP responses
 @AllArgsConstructor
 @Data
 public class ApiResponse {
