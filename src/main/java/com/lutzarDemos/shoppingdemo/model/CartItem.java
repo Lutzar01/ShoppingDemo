d56eac266aeff77596dadba12c9d3479eecf7594
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-04 19:41:10 -0700

    Create CartItem.java
    CartItem.java is the model entity for a cart item

---
+++ src/main/java/com/lutzarDemos/shoppingdemo/model/CartItem.java
@@ -0,0 +1,40 @@
+package com.lutzarDemos.shoppingdemo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+// Model to define each cart item (ie. product)
+// You can have multiple of the same cart item
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+
+    // Product in the cart item
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    // Cart the cart item is in
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    public void setTotalPrice() {
+        this.totalPrice = this.unitPrice.multiply(new BigDecimal(quantity));
+    }
+}
