4e63d64b890b69297c429d5be800c0ff9f55ebf4
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-04 19:56:59 -0700

    Create ICartService.java
    base methods created:
    - getCart: takes in cart_id
    - clearCart: takes in cart_id
    - getTotalPrice: takes in cart_id

---
+++ src/main/java/com/lutzarDemos/shoppingdemo/service/cart/ICartService.java
@@ -0,0 +1,12 @@
+package com.lutzarDemos.shoppingdemo.service.cart;
+
+import com.lutzarDemos.shoppingdemo.model.Cart;
+
+import java.math.BigDecimal;
+
+// Contains base methods relating to the cart entity for business logic and application functionality
+public interface ICartService {
+    Cart getCart(Long id);
+    void clearCart(Long id);
+    BigDecimal getTotalPrice(Long id);
+}
