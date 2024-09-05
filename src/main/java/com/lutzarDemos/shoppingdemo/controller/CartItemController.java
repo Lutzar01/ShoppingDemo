f554d4f7883183b6f54ff90536800df28ae0f03a
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-04 21:56:03 -0700

    Create CartItemController.java
    Created HTTP requests:
    - addItemToCart: Takes in {cartId, productId, quantity} and returns response
    - removeItemFromCart: Takes in {cartId, productId} and returns response
    - updateItemQuantity: Takes in {cartId, productId, quantity} and returns response

---
+++ src/main/java/com/lutzarDemos/shoppingdemo/controller/CartItemController.java
@@ -0,0 +1,63 @@
+package com.lutzarDemos.shoppingdemo.controller;
+
+import com.lutzarDemos.shoppingdemo.exceptions.ResourceNotFoundException;
+import com.lutzarDemos.shoppingdemo.response.ApiResponse;
+import com.lutzarDemos.shoppingdemo.service.cart.ICartItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+// Handles HTTP requests and returns a response for carts
+// otherwise returns HTTP status error
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/cartItems")
+public class CartItemController {
+    private final ICartItemService cartItemService;
+
+    // Handles HTTP request to add a CartItem
+    // Returns response "Add Item Success"
+    @PostMapping("/item/add")
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Long cartId,
+                                                     @RequestParam Long productId,
+                                                     @RequestParam Integer quantity) {
+        try {
+            cartItemService.addItemToCart(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Add Item Success!", null));
+
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    // Handles HTTP request to remove an existing Product from an existing Cart
+    // returns the response "Item Removed!"
+    @DeleteMapping("/cart/{cartId}/product/{productId}/remove")
+    public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable Long cartId,
+                                                          @PathVariable Long productId) {
+        try {
+            cartItemService.removeItemFromCart(cartId, productId);
+            return ResponseEntity.ok(new ApiResponse("Item Removed!", null));
+
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    // Handles HTTP request to update an existing Product quantity in an existing Cart
+    // returns the response "Update Item Success!" 
+    @PutMapping("/cart/{cartId}/product/{productId}/update")
+    public ResponseEntity<ApiResponse> updateItemQuantity(@PathVariable Long cartId,
+                                                          @PathVariable Long productId,
+                                                          @RequestParam Integer quantity) {
+        try {
+            cartItemService.updateItemQuantity(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Update Item Success!", null));
+
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
