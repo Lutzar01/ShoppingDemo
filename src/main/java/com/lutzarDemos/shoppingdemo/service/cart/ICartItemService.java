67005c3cad9956a524889bfb514c45fcbf220a33
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-04 20:39:26 -0700

    Update ICartItemService.java
    removed cart_item from addItemToCart method

--- src/main/java/com/lutzarDemos/shoppingdemo/service/cart/ICartItemService.java
+++ src/main/java/com/lutzarDemos/shoppingdemo/service/cart/ICartItemService.java
@@ -4,7 +4,7 @@ import com.lutzarDemos.shoppingdemo.model.CartItem;
 
 // Contains base methods relating to the cart item entity for business logic and application functionality
 public interface ICartItemService {
-    void addItemToCart(Long cartId, Long productId, int quantity, CartItem cartItem);
+    void addItemToCart(Long cartId, Long productId, int quantity);
     void removeItemFromCart(Long cartId, Long productId);
     void updateItemQuantity(Long cartId, Long productId, int quantity);
 }
