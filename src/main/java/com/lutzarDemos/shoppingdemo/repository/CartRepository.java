26e362b91d930c33699ec642f3ec2ed4c3d3ac18
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-04 20:00:50 -0700

    Create CartRepository.java
    needed to complete CartService.java

---
+++ src/main/java/com/lutzarDemos/shoppingdemo/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package com.lutzarDemos.shoppingdemo.repository;
+
+import com.lutzarDemos.shoppingdemo.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Manages cart entity and saves data to the MySQL DB
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
