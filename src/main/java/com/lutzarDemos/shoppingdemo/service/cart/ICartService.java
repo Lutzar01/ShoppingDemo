7bbb95e26a21412f221d216462209aedd86d88ff
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-07 22:24:24 -0700

    Feat & Clean: update documentation & base methods
    - updated documentation to reflect javadoc styling (see #51)
    - added base method for getting a CART by the USER's id

--- src/main/java/com/lutzarDemos/shoppingdemo/service/cart/ICartService.java
+++ src/main/java/com/lutzarDemos/shoppingdemo/service/cart/ICartService.java
@@ -4,11 +4,19 @@ import com.lutzarDemos.shoppingdemo.model.Cart;
 
 import java.math.BigDecimal;
 
-// Contains base methods relating to the cart entity for business logic and application functionality
+/**
+ * Contains base methods relating to the CART entity
+ * for business logic and application functionality
+ *
+ * @author      Lutzar
+ * @version     1.2, 2024/09/07
+ */
 public interface ICartService {
     Cart getCart(Long id);
     void clearCart(Long id);
     BigDecimal getTotalPrice(Long id);
 
     Long initializeNewCart();
+
+    Cart getCartByUserId(Long userId);
 }
