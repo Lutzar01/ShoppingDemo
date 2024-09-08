0eb9933fa0f21d6970c6b831fd5cdfb578dde58e
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-07 22:35:46 -0700

    Feat: update with new method to find a list of saved orders by Id
    A user can have many ORDERS that they make at once with different statuses and ORDER ITEMS, this allows the USER to see all of their orders that have been placed.

--- src/main/java/com/lutzarDemos/shoppingdemo/repository/OrderRepository.java
+++ src/main/java/com/lutzarDemos/shoppingdemo/repository/OrderRepository.java
@@ -3,11 +3,14 @@ package com.lutzarDemos.shoppingdemo.repository;
 import com.lutzarDemos.shoppingdemo.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Manages ORDER entity and saves data to the MySQL DB
  *
  * @author      Lutzar
- * @version     1.1, 2024/09/07
+ * @version     1.2, 2024/09/07
  */
 public interface OrderRepository extends JpaRepository<Order, Long>{
+    List<Order> findByUserId(Long userId);
 }
