e37363dc0391c085551237092f8885e485446185
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-09-11 20:19:53 -0700

    Feat: Added "brandName" as a param to be included in the OrderItemDto class
    updated versioning

--- src/main/java/com/lutzarDemos/shoppingdemo/dto/OrderItemDto.java
+++ src/main/java/com/lutzarDemos/shoppingdemo/dto/OrderItemDto.java
@@ -8,12 +8,13 @@ import java.math.BigDecimal;
  * Data Transfer Object for ORDER ITEM entity
  *
  * @author      Lutzar
- * @version     1.1, 2024/09/10
+ * @version     1.2, 2024/09/10
  */
 @Data
 public class OrderItemDto {
     private Long productId;
     private String productName;
+    private String brandName;
     private int quantity;
     private BigDecimal price;
 }
